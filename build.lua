51c5946a78a304dbd3bd3b65f34e7cf6c986796e
Will Robertson <wspr81@gmail.com>
2018-12-07 13:42:19 +1030

    metadata for l3build

--- build.lua
+++ build.lua
@@ -1,7 +1,22 @@
 
-module = "xetexref"
+metadata = {
+  pkg         = "xetexref",
+  version     = "2018-12-07",
+  author      = "Will Robertson",
+  license     = "lppl1.3c",
+  summary     = "Reference documentation for XeTeX",
+  ctanPath    = "/info/xetexref",
+  bugtracker  = "https://github.com/wspr/xetexref/issues",
+  support     = "http://lists.tug.org/xetex",
+  repository  = "https://github.com/wspr/xetexref/",
+  description = "This package comprises reference documentation for the XeTeX engine, detailing its extended features.",
+  update      = true,
+}
 
+module = metadata.pkg
 typesetfiles = {"xetex-reference.tex"}
 typesetexe = "xelatex -no-mktex=tfm "
 
-ctan_debug = true
+ctandata = metadata
+require("l3build-wspr.lua")
+ctanupload = true
