044a0b5cf68cb6d49ca5efb4e1eda3c363828986
Will Robertson <wspr81@gmail.com>
2019-12-09 11:30:35 +1030

    build.lua and copyright

--- build.lua
+++ build.lua
@@ -1,20 +1,35 @@
+--[[
+     =======================
+     BUILD FILE FOR XETEXREF
+     =======================
+--]]
 
 module = "xetexref"
 
 uploadconfig = {
   pkg         = module,
-  version     = "2019-03-05",
+  version     = "2019-12-09",
   author      = "Will Robertson",
   license     = "lppl1.3c",
   summary     = "Reference documentation for XeTeX",
   ctanPath    = "/info/xetexref",
-  bugtracker  = "https://github.com/wspr/xetexref/issues",
   support     = "http://lists.tug.org/xetex",
   repository  = "https://github.com/wspr/xetexref/",
-  description = "This package comprises reference documentation for the XeTeX engine, detailing its extended features.",
+  bugtracker  = "https://github.com/wspr/xetexref/issues",
 }
 
-typesetfiles = {"xetex-reference.tex"}
-typesetexe = "xelatex -no-mktex=tfm "
+uploadconfig.description = [[
+  This package comprises reference documentation for the XeTeX engine,
+  detailing its extended features.
+]]
 
-require("l3build-wspr.lua")
+local function prequire(m) -- from: https://stackoverflow.com/a/17878208
+  local ok, err = pcall(require, m)
+  if not ok then return nil, err end
+  return err
+end
+prequire("l3build-wspr.lua")
+
+typesetfiles = {"xetex-reference.tex"}
+typesetexe   = "xelatex -no-mktex=tfm"
+typesetcmds  = "\\\\def\\\\VERSION{"..uploadconfig.version.."}"
